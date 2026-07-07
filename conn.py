35b50aa4debdee430f4ed572ae7d34ddf309c445
S-Preeti <sripreethi.krishna@gmail.com>
2026-07-07 17:47:51 +0100

    Remove conn.py with credentials from tracking

--- conn.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-conn = pyodbc.connect(
-    "Driver={ODBC Driver 18 for SQL Server};"
-    "Server=synapse-ukretail-workspace-ondemand.sql.azuresynapse.net;"
-    "Database=UKRetailDWH;"
-    "UID=sqladmin;PWD=Warmlite@123;"
-    "Timeout=30;"
-)
-print("Connected!")
